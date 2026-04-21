508d066094f2a20684870dda7945dbea06167f47
omarAlaa <omarmuhamed10@gmail.com>
2026-04-21 18:17:21 +0200

    change form to controlled form

--- src/features/products/components/AddProductForm/AddProductForm.jsx
+++ src/features/products/components/AddProductForm/AddProductForm.jsx
@@ -12,33 +12,33 @@ import Button from '../../../../shared/ui/Button/Button'
 
 export default function AddProductForm() {
 
-    const formRef = useRef()
     const { setShowCatModal, categories } = categoriesStore()
     const { addNewProduct } = productsStore()
     const { showSnackBar } = uiStore()
+    const [title, setTitle] = useState('')
     const [category, setCategory] = useState('')
+    const [price, setPrice] = useState('')
+    const [image, setImageUrl] = useState('')
+    const [description, setDescription] = useState('')
     const [addProductLoading, setAddProductLoading] = useState(false)
+    const addDisabled = addProductLoading || !title || !price || !category || !description || !image
 
     const handleAddProduct = async (e) => {
         e.preventDefault()
-        const data = new FormData(e.currentTarget)
+
         setAddProductLoading(true)
 
-        if (!data.get('title') || !data.get('price') || !data.get('category') || !data.get('description') || !data.get('image-url')) {
-            showSnackBar({ visible: true, success: false, text: 'You must fill all the fields' })
-
-            setAddProductLoading(false)
-            return
-        }
-
         try {
-            const res = await addProduct({ title: data.get('title'), price: data.get('price'), description: data.get('description'), category: data.get('category'), image: data.get('image-url') })
+            const res = await addProduct({ title, price, description, category, image })
             addNewProduct(res.data)
 
             showSnackBar({ visible: true, success: true, text: 'Product Added' })
 
+            setTitle('')
             setCategory('')
-            formRef.current.reset()
+            setPrice('')
+            setImageUrl('')
+            setDescription('')
         } catch (error) {
             const errorMessage = error?.response?.data?.message || 'Failed to add product'
             showSnackBar({ visible: true, success: false, text: errorMessage })
@@ -48,11 +48,11 @@ export default function AddProductForm() {
     }
 
     return (
-        <form ref={formRef} onSubmit={handleAddProduct} className={styles.addProduct}>
-            <Input type="text" name="title" id="product-title" placeholder="Title" />
+        <form onSubmit={handleAddProduct} className={styles.addProduct}>
+            <Input type="text" name="title" id="product-title" placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
 
             <div className={styles.oneRow}>
-                <select className={styles.select} name="category" id="category" defaultValue={''} onChange={e => setCategory(e.target.value)} style={{ color: category ? 'black' : 'gray' }}>
+                <select className={category ? styles.select : styles.graySelect} name="category" id="category" value={category} onChange={e => setCategory(e.target.value)}>
                     <option value="" disabled >Select Category</option>
 
                     {categories?.map(category => <option key={category._id} className={styles.option} value={category.name}>{category.name}</option>)}
@@ -68,16 +68,15 @@ export default function AddProductForm() {
             </div>
 
             <div className={styles.oneRow}>
-                <Input type="number" name="price" id="price" placeholder="Price" step="0.01" min="0" />
+                <Input type="number" name="price" id="price" placeholder="Price" step="0.01" min="0" value={price} onChange={e => setPrice(e.target.value)} />
 
-                <Input type="text" name="image-url" id="image-url" placeholder="Image URL" />
+                <Input type="text" name="image-url" id="image-url" placeholder="Image URL" value={image} onChange={e => setImageUrl(e.target.value)} />
             </div>
 
-            <textarea className={styles.textarea} type="text" name="description" id="description" placeholder="Description" />
+            <textarea className={styles.textarea} type="text" name="description" id="description" placeholder="Description" value={description} onChange={e => setDescription(e.target.value)} />
 
-            <Button id={styles.newProduct}
-                type='submit'
-                disabled={addProductLoading}>
+            <Button id={!addDisabled ? styles.newProduct : undefined}
+                disabled={addDisabled}>
                 {addProductLoading ? <Loading size={18} height={'100%'} /> : '+ Add Product'}
             </Button>
 
